7f11ce0dd4ef50af5e7ca73492a93959559187be
affandeshmukh <69684368+affandeshmukh@users.noreply.github.com>
2022-02-02 13:01:03 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.videocalling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.jitsi.meet.sdk.JitsiMeet;
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    EditText secretcode;
+    Button join;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        secretcode=findViewById(R.id.editTextTextPersonName);
+        join =findViewById(R.id.button);
+        URL server;
+        try{
+            server =new URL("https://meet.jit.si");
+            JitsiMeetConferenceOptions defaultOptions=
+                    new JitsiMeetConferenceOptions.Builder()
+                    .setServerURL(server)
+                    .setWelcomePageEnabled(false)
+                    .build();
+            JitsiMeet.setDefaultConferenceOptions(defaultOptions);
+
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+        join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JitsiMeetConferenceOptions options = new JitsiMeetConferenceOptions.Builder()
+                        .setRoom(secretcode.getText().toString())
+                        .setWelcomePageEnabled(false).build();
+                JitsiMeetActivity.launch(MainActivity.this,options);
+            }
+        });
+    }
+}
